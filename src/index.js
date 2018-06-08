c80c9adfbdacab9954be0eb3eba36110ed9450e7
klequis <klequis@gmail.com>
2018-06-07 19:18:17 -0700

    import of new typefaces

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store'
 import AppWrapper from './App/AppWrapper'
+import 'typeface-roboto'
+import 'typeface-roboto-condensed'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
