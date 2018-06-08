d83c1aba5d3ba7f1f6b3ef6a099cd3b2019c85e9
klequis <klequis@gmail.com>
2018-06-07 19:12:38 -0700

    added more theme properties.

--- src/withRoot.js
+++ src/withRoot.js
@@ -12,18 +12,94 @@ const theme = createMuiTheme({
     primary: blue,
   },
   typography: {
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    display4: {
+      fontSize: "7rem",
+      fontWeight: 300,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      letterSpacing: "-.04em",
+      lineHeight: "1.14286em",
+      marginLeft: "-.04em",
+      // color: "rgba(0, 0, 0, 0.54)",
+    },
+    display3: {
+      fontSize: "3.5rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      letterSpacing: "-.02em",
+      lineHeight: "1.30357em",
+      marginLeft: "-.02em",
+      // color: "rgba(0, 0, 0, 0.54)",
+    },
+    display2: {
+      fontSize: "2.8125rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.06667em",
+      marginLeft: "-.02em",
+      // color: "rgba(0, 0, 0, 0.54)",
+    },
+    display1: {
+      fontSize: "2.125rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.20588em",
+      // color: "rgba(0, 0, 0, 0.54)",
+    },
     headline: {
-      fontSize: '0.937em',
+      fontSize: "1.5rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.35417em",
+      // color: "rgba(0, 0, 0, 0.87)",
+    },
+    title: {
+      fontSize: "1.3125rem",
+      fontWeight: 500,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.16667em",
+      // color: "rgba(0, 0, 0, 0.87)",
     },
     subheading: {
-      fontSize: '0.837em',
+      fontSize: "1rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.5em",
+      // color: "rgba(0, 0, 0, 0.87)",
+    },
+    body2: {
+      fontSize: "0.875rem",
+      fontWeight: 500,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.71429em",
+      // color: "rgba(0, 0, 0, 0.87)",
+    },
+    body1: {
+      fontSize: "0.875rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.46429em",
+      // color: "rgba(0, 0, 0, 0.87)",
+    },
+    caption: {
+      fontSize: "0.75rem",
+      fontWeight: 400,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      lineHeight: "1.375em",
+      // color: "rgba(0, 0, 0, 0.54)",
+    },
+    button: {
+      fontSize: "0.875rem",
+      textTransform: "uppercase",
+      fontWeight: 500,
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      // color: "rgba(0, 0, 0, 0.87)",
     }
   }
-  // typography: {
-  //   display1: {
-  //     fontSize: '3rem',
-  //   }
-  // }
 })
 
 function withRoot(Component) {
