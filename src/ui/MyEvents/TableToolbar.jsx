d9fe67bdbf89977db580c03056c44bfc3472a840
klequis <klequis@gmail.com>
2018-07-05 05:49:33 -0700

    Edit click is loading NewEvent but no load yet

--- src/ui/MyEvents/TableToolbar.jsx
+++ src/ui/MyEvents/TableToolbar.jsx
@@ -49,72 +49,89 @@ const styles = theme => ({
 })
 
 
-const handleEditClick = (event, _id) => {
-  /* 
-      Use react reactRouter <Link/> component passing it the _id.
-      The link will be to NewEvent
-      Having received an _id, NewEvent will initialize values
-  */
-  green('handleEditClick: _id', _id)
-  return (
-    <Redirect to={`/new-event/:${_id}`}/>
-  )
-}
 
-let TableToolbar = props => {
-  const { selected, classes } = props
-  const numSelected = selected.length
-  green('TableToobar: props', props)
-  let _id = ''
-  if (numSelected === 1) {
-    green('**num = 1')
-    green('if selected', selected[0])
-    _id = selected[0]
-    green('_id', _id)
+class TableToolbar extends React.Component {
+  state = {
+    selectedId: '',
+    editClicked: false,
   }
+  
+  handleEditClick = (event) => {
+    /* 
+        Use react reactRouter <Link/> component passing it the _id.
+        The link will be to NewEvent
+        Having received an _id, NewEvent will initialize values
+    */
+    const { selected } = this.props
+    
+    let _id = ''
+    if (selected.length === 1) {
+      this.setState({
+        selectedId: selected[0],
+        editClicked: true,
+      })
+    }
+    
+    green('handleEditClick: _id', _id)
+    return <Redirect to={`/new-event/:${_id}`}/>
+    
+  }
+  
+  
+  render() {
+    const { selected, classes } = this.props
+    const numSelected = selected.length
+    green('TableToobar: this.props', this.props)
+    const { selectedId, editClicked } = this.state
+    if (selectedId && editClicked) {
+      return <Redirect to={`/new-event/${selectedId}`}/>
+    }
+    
 
-  return (
-    <Toolbar
-      className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      <div className={classes.title}>
-        {numSelected > 0 ? (
-          <Typography color="inherit" variant="subheading">
-            {numSelected} selected
-          </Typography>
-        ) : (
-          <Typography variant="title" id="tableTitle">
-            Test's Events
-          </Typography>
-        )}
-      </div>
-      <div className={classes.spacer} />
-      <div className={classes.actions}>
-        {numSelected > 0 ? (
-          <div className={classes.editDeleteGroup}>
-            <Tooltip title="Edit">
-              <IconButton aria-label="Edit" disabled={numSelected > 1} onClick={e => handleEditClick(e, _id)}>
-                <EditIcon />
+    return (
+      <Toolbar
+        className={classNames(classes.root, {
+          [classes.highlight]: numSelected > 0,
+        })}
+      >
+        <div className={classes.title}>
+          {numSelected > 0 ? (
+            <Typography color="inherit" variant="subheading">
+              {numSelected} selected
+            </Typography>
+          ) : (
+            <Typography variant="title" id="tableTitle">
+              Test's Events
+            </Typography>
+          )}
+        </div>
+        <div className={classes.spacer} />
+        <div className={classes.actions}>
+          {numSelected > 0 ? (
+            <div className={classes.editDeleteGroup}>
+              <Tooltip title="Edit">
+                <IconButton aria-label="Edit" disabled={numSelected > 1} onClick={e => this.handleEditClick(e)}>
+                  <EditIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Delete">
+                <IconButton aria-label="Delete">
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          ) : (
+            <Tooltip title="Filter list">
+              <IconButton aria-label="Filter list">
+                <FilterListIcon />
               </IconButton>
             </Tooltip>
-            <Tooltip title="Delete">
-              <IconButton aria-label="Delete">
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          </div>
-        ) : (
-          <Tooltip title="Filter list">
-            <IconButton aria-label="Filter list">
-              <FilterListIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-      </div>
-    </Toolbar>
-  )
+          )}
+        </div>
+      </Toolbar>
+    )
+  }
+  
 }
 
 TableToolbar.propTypes = {
