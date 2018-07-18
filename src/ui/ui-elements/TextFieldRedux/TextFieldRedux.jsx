dbadd7f52f6c8fb48e0862e3cd80edc0b8ef519e
klequis <klequis@gmail.com>
2018-07-18 06:24:22 -0700

    Made title multi-line and simplified component

--- src/ui/ui-elements/TextFieldRedux/TextFieldRedux.jsx
+++ src/ui/ui-elements/TextFieldRedux/TextFieldRedux.jsx
@@ -4,19 +4,23 @@ import { Field } from 'redux-form'
 import { TextField } from '@material-ui/core'
 import FormControl from '@material-ui/core/FormControl'
 
+
 /* Dev */
-// import { green } from 'logger'
+// eslint-disable-next-line
+import { green } from 'logger'
 
 /*
     error: in redux-form, error is a text field. In mui it is a boolean
 */
-const renderTextField = (
-  { input, label, meta, ...custom },
-) => {
+const renderTextField = (props) => {
+  green('TextFieldRedux.renderTextField: props', props)
+  green('** rows', props.meta.rows)
+  const { input, label, meta, ...custom } = props
   const { error, touched } = meta
-  const { disabled, ...customRest } = custom
+  const { disabled, rows, ...customRest } = custom
   const hasErrorSet = typeof error === 'undefined' ? false : true
   const isError = hasErrorSet && touched
+
   return (
     <TextField
       disabled={disabled}
@@ -24,7 +28,7 @@ const renderTextField = (
       helperText={touched && error}
       label={label}
       placeholder={label}
-
+      rows={rows}
       {...input}
       {...customRest}
     />
@@ -34,30 +38,21 @@ const renderTextField = (
 const TextFieldRedux = props => {
   const { fieldName, fieldLabel, disabled, required, fullWidth=false, rows=0 } = props
   const multilineField = rows > 1
+  green('TextFieldRedux', multilineField)
 
   return (
+
     <FormControl fullWidth={fullWidth}>
-      {
-        multilineField
-          ? <Field 
-              component={renderTextField}
-              disabled={disabled}
-              fullWidth={fullWidth}
-              label={fieldLabel}
-              multiline={rows > 1}
-              name={fieldName} 
-              required={required}
-              rows={rows}
-            />
-          : <Field 
-              component={renderTextField}
-              disabled={disabled}
-              fullWidth
-              label={fieldLabel}
-              name={fieldName} 
-              required={required}
-            />
-      }
+      <Field 
+        component={renderTextField}
+        disabled={disabled}
+        fullWidth={fullWidth}
+        label={fieldLabel}
+        multiline={multilineField}
+        name={fieldName} 
+        required={required}
+        rows={rows}
+      />
     </FormControl>
   )
 }
