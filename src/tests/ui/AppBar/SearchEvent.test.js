6f9d0f5a2cbb8bc0ec8fa57778f888d5cac65605
Hemalatha Manickam <hmanicka@gmail.com>
2018-07-26 09:13:27 -0700

    added more tests for SearchEvent component

--- src/tests/ui/AppBar/SearchEvent.test.js
+++ src/tests/ui/AppBar/SearchEvent.test.js
@@ -1,28 +1,47 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { TextField } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 import { SearchEvent } from '../../../ui/AppBar/SearchEvent'
 import styles from '../../../ui/AppBar/styles'
 
 const Composer = ({
-  classes,
+  classes
 }) => (
-  <SearchEvent classes={classes} />
+  <SearchEvent classes={classes}/>
 )
 
 Composer.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 }
 
 const Composition = withStyles(styles)(Composer)
 
 describe('<SearchEvent />', () => {
-  it('should render a styled SearchEvent', () => {
-    const wrapper = shallow(<Composition />)
-    // console.log('wrapper: ', wrapper.dive().html())
-    // Note the use of dive() because Composition is now wrapped by the withStyles higher order component.
-    expect(wrapper.dive().find(SearchEvent)).toHaveLength(1)
+
+  let wrapper
+
+  beforeAll(() => {
+    wrapper = mount(<Composition />)
   })
+
+  test('should render SearchEvent correctly', () => {
+    expect(wrapper.find(SearchEvent)).toHaveLength(1)
+  })
+
+  test('should handle onChange on search term textfield', async() => {
+    const term = 'briia'
+    const termInput = wrapper.find(TextField).find('input')
+
+    // Method 2: sends the term but the state is not updated as it is async
+    termInput.simulate('change', {
+      persist: () => {},
+      target: { value: term }
+    })
+
+    expect(wrapper.find(TextField).prop('value')).toEqual(term)
+  })
+
 })
