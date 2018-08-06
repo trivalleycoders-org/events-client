174c4f0fecf8c7442088c4e3b9a2441f18b8ccf9
Hemalatha Manickam <hmanicka@gmail.com>
2018-08-06 13:07:36 -0700

    added test stubs for event form

---
+++ src/tests/ui/EventForm/EventForm.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+describe('Event Form', () => {
+
+  beforeAll(() => {
+
+  })
+
+  it('should render Event Form correctly', () => {
+
+  })
+
+  it('should render all the children correctly', () => {
+
+  })
+
+  describe('when the submit button is clicked', () => {
+    it('the form should submit if all the required fields are input', () => {
+
+    })
+
+    it('the form should not submit if one or more of the required fields are not input', () => {
+
+    })
+
+  })
+})
