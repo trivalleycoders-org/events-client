5dd24181e7125d81c05c6cd840b1236d19f04ad1
klequis <klequis@gmail.com>
2018-08-27 10:14:20 -0700

    -wip

--- src/ui/AppMenu/AppMenu.jsx
+++ src/ui/AppMenu/AppMenu.jsx
@@ -8,8 +8,10 @@ import Button from '@material-ui/core/Button'
 import List from '@material-ui/core/List'
 import Divider from '@material-ui/core/Divider'
 import { mailFolderListItems, otherMailFolderListItems } from './tileData'
+import { menuItems } from './tileData'
 import * as appMenuActions from 'store/actions/app-menu-actions'
 import * as appMenuSelectors from 'store/selectors/app-menu-selectors'
+import SearchEvent from '../AppBar/SearchEvent'
 /* Dev */
 // eslint-disable-next-line
 import { green } from 'logger'
@@ -34,70 +36,40 @@ class AppMenu extends React.Component {
     const { classes } = this.props
     green('AppMenu: props', this.props)
 
-    const sideList = (
+    // const sideList = (
+    //   <div className={classes.list}>
+    //     <List>{menuItems}</List>
+    //     <Divider />
+    //     <List>{otherMailFolderListItems}</List>
+    //   </div>
+    // )
+
+    const renderMenuItems = (
       <div className={classes.list}>
-        <List>{mailFolderListItems}</List>
-        <Divider />
-        <List>{otherMailFolderListItems}</List>
+        <List>{menuItems}</List>
       </div>
     )
 
-    const fullList = (
-      <div className={classes.fullList}>
-        <List>{mailFolderListItems}</List>
-        <Divider />
-        <List>{otherMailFolderListItems}</List>
-      </div>
-    )
+    // const fullList = (
+    //   <div className={classes.fullList}>
+    //     <List>{mailFolderListItems}</List>
+    //     <Divider />
+    //     <List>{otherMailFolderListItems}</List>
+    //   </div>
+    // )
 
     return (
       <div>
-        <Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>
-        <Button onClick={this.toggleDrawer('right', true)}>Open Right</Button>
-        <Button onClick={this.toggleDrawer('top', true)}>Open Top</Button>
-        <Button onClick={this.toggleDrawer('bottom', true)}>Open Bottom</Button>
-        <Drawer open={this.props.appMenuState} onClose={this.toggleDrawer('left', false)}>
+        <Drawer open={this.props.appMenuState} onClose= {this.toggleDrawer('left', false)}>
+          <SearchEvent/>
           <div
             tabIndex={0}
             role="button"
             onClick={this.toggleDrawer('left', false)}
             onKeyDown={this.toggleDrawer('left', false)}
           >
-            {sideList}
-          </div>
-        </Drawer>
-        <Drawer anchor="top" open={this.state.top} onClose={this.toggleDrawer('top', false)}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.toggleDrawer('top', false)}
-            onKeyDown={this.toggleDrawer('top', false)}
-          >
-            {fullList}
-          </div>
-        </Drawer>
-        <Drawer
-          anchor="bottom"
-          open={this.state.bottom}
-          onClose={this.toggleDrawer('bottom', false)}
-        >
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.toggleDrawer('bottom', false)}
-            onKeyDown={this.toggleDrawer('bottom', false)}
-          >
-            {fullList}
-          </div>
-        </Drawer>
-        <Drawer anchor="right" open={this.state.right} onClose={this.toggleDrawer('right', false)}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.toggleDrawer('right', false)}
-            onKeyDown={this.toggleDrawer('right', false)}
-          >
-            {sideList}
+
+            {renderMenuItems}
           </div>
         </Drawer>
       </div>
--- src/ui/AppMenu/tileData.js
+++ src/ui/AppMenu/tileData.js
@@ -1,6 +1,7 @@
 // This file is shared across the demos.
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
@@ -11,6 +12,65 @@ import SendIcon from '@material-ui/icons/Send'
 import MailIcon from '@material-ui/icons/Mail'
 import DeleteIcon from '@material-ui/icons/Delete'
 import ReportIcon from '@material-ui/icons/Report'
+
+import { Home, Event } from '@material-ui/icons'
+
+const linkStyle = {
+  textDecoration: 'none'
+}
+
+export const menuItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <Home />
+      </ListItemIcon>
+      <Link to='/' style={linkStyle}>
+        <ListItemText primary='Home' />
+      </Link>
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <Event />
+      </ListItemIcon>
+      <Link to='/new-event' style={linkStyle}>
+        <ListItemText primary='New Event' />
+      </Link>
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <Link to='/my-events' style={linkStyle}>
+        <ListItemText primary='My Events' />
+      </Link>
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <Link to='/register' style={linkStyle}>
+        <ListItemText primary='Register' />
+      </Link>
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <Link to='/login' style={linkStyle}>
+        <ListItemText primary='Login' />
+      </Link>
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <Link to='/settings' style={linkStyle}>
+        <ListItemText primary='Settings' />
+      </Link>
+    </ListItem>
+  </div>
+)
 
 export const mailFolderListItems = (
   <div>
