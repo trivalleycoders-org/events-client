cd1e9bb9d14f77ffc3e7f4537af1e7cb7ac9ec23
klequis <klequis@gmail.com>
2018-08-30 10:13:29 -0700

    initial

---
+++ src/lib/formattedDate.js
@@ -0,0 +1,34 @@
+/* Dev */
+// eslint-disable-next-line
+import { green } from 'logger'
+
+const formatMinutes = (minutes) => {
+  const mins = Number(minutes).toString()
+  green('mins', mins)
+  green('mins.length', mins.length)
+  if (mins.length === 1) {
+    // green('with zero', `0${mins}`)
+    return mins.padStart(2, '0')
+  }
+  return mins
+}
+
+const hourAmPm = (date) => {
+  const h = date.getHours()
+  const m = date.getMinutes().toString().padStart(2, 0)
+  green('m', m)
+  return (h > 12)
+    ? `${h-12}:${m} PM`
+    : `${h}:${m} AM`
+}
+
+export const formattedDate = (isoDateString) => {
+  const dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+  const monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+  const d = new Date(isoDateString)
+  const MMM = monthNames[d.getMonth()]
+  const DDD = dayNames[d.getDay()]
+  const dd = d.getDate()
+  const hour = hourAmPm(d)
+  return `${DDD}, ${MMM} ${dd}, ${hour}`
+}
