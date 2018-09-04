a195c984dba751a572bcce15b5b2a2aa9af4499c
klequis <klequis@gmail.com>
2018-09-03 17:41:10 -0700

    modified to handle nested props

--- src/lib/hasProp.js
+++ src/lib/hasProp.js
@@ -1,4 +1,14 @@
+import { path } from 'ramda'
+
+/*
+    takes a . delimeted path as string
+    such as: 'cat.dog.camel'
+    or: 'horse'
+*/
+
 export const hasProp = (prop, obj) => {
-  const ret = obj.hasOwnProperty(prop)
-  return ret
+  return path(prop.split('.'), obj) === undefined
+    ? false
+    : true
+  //
 }
