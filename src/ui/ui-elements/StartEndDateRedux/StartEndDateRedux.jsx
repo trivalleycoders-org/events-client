591cf6692169634c97e1c66d61092d59ec6b9274
klequis <klequis@gmail.com>
2018-09-13 19:11:43 -0700

    fixed issue with date format

--- src/ui/ui-elements/StartEndDateRedux/StartEndDateRedux.jsx
+++ src/ui/ui-elements/StartEndDateRedux/StartEndDateRedux.jsx
@@ -4,26 +4,27 @@ import Combined from './Combined'
 import { Field } from 'redux-form'
 /* Dev */
 // eslint-disable-next-line
-import { green } from '../../../logger/index'
-// import { green } from 'logger'
+import { green } from 'logger'
 
 class StartEndDateRedux extends React.Component {
 
   picker = (props) => {
+    green('date props', props)
     const { onChange, ...rest } = props.input
     return (
       <Combined
         {...rest}
         onChange={onChange}
-        format='MMM DD YYYY hh:mm A'
+        format={props.dateFormat}
         fullWidth
         initial={props.meta.initial}
         // required={true}
       />
     )
   }
+  // format='MMM do YYYY hh:mm a'
   render() {
-    const { fieldLabel, fieldName } = this.props
+    const { fieldLabel, fieldName, dateFormat='MMM do YYYY hh:mm a' } = this.props
 
     return (
       <Field
@@ -31,6 +32,7 @@ class StartEndDateRedux extends React.Component {
         fullWidth
         name={fieldName}
         label={fieldLabel}
+        dateFormat={dateFormat}
         // required={true}
 
         // validate={() => console.log('validate')}
