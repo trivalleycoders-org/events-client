37522dd2b701ed54c91aef78837702bfd28201c5
klequis <klequis@gmail.com>
2018-09-18 12:52:29 -0700

    added prop 'enableEdit'  t/f to enable/disable editing

--- src/ui/ui-elements/UploadImage/UploadImage.jsx
+++ src/ui/ui-elements/UploadImage/UploadImage.jsx
@@ -2,27 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Component from './Component'
 import { Field } from 'redux-form'
-// import { green } from 'logger'
+// eslint-disable-next-line
+import { green } from 'logger'
 
 class UploadImage extends React.Component {
 
   picker = (props) => {
+    green('picker props', props)
     const { onChange, ...rest } = props.input
+    const { initial } = props.meta
+    const { enableEdit } = props
+    green('rest', rest)
     return (
       <Component
         {...rest}
         onChange={onChange}
-        initial={props.meta.initial}
+        initial={initial}
+        enableEdit={enableEdit}
       />
     )
   }
   render() {
-    const { fieldLabel, fieldName } = this.props
+    const { fieldLabel, fieldName, enableEdit } = this.props
+    green('enableEdit', enableEdit)
     return (
       <Field
         component={this.picker}
         name={fieldName}
         label={fieldLabel}
+        props={{
+          enableEdit: enableEdit
+        }}
       />
     )
   }
