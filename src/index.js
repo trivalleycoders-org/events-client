3d96dd35463f652232f03011dd2300b67bb318e8
klequis <klequis@gmail.com>
2018-10-22 16:00:18 -0700

    Create <AppContainer />

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store'
 import App from './ui/App'
@@ -8,7 +8,7 @@ import App from './ui/App'
 ReactDOM.render(
   <Provider store={configureStore()}>
     <Router>
-      <App />
+      <Route component={App} />
     </Router>
   </Provider>,
   document.getElementById('root')
